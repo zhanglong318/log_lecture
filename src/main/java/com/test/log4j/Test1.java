65382905605fd02dc7fdb01d4225007342420c07
张龙 <zhanglong@wusongtech.com>
2016-03-27 21:01:34 +0800

    initial commit

---
+++ src/main/java/com/test/log4j/Test1.java
@@ -0,0 +1,12 @@
+package com.test.log4j;
+
+
+import org.apache.log4j.Logger;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Test1.class);
+        logger.info("hello world");
+    }
+}
