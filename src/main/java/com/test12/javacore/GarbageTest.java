be715b1825073faadcaf4f51123e706bae3d24da
张龙 <zhanglong@wusongtech.com>
2016-03-30 01:59:43 +0800

    javacore and heapdump

--- src/main/java/com/test12/javacore/GarbageTest.java
+++ src/main/java/com/test12/javacore/GarbageTest.java
@@ -1,7 +1,28 @@
 package com.test12.javacore;
 
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * Created by zhanglong on 16/3/30.
+ * -Xms10m -Xmx10m -XX:+HeapDumpOnOutOfMemoryError
+ *
+ * @author zhanglong
+ *
  */
+
 public class GarbageTest {
+
+    public static void main(String[] args) {
+
+        List<GarbageTest> list = new LinkedList<>();
+
+        for(;;)
+        {
+            list.add(new GarbageTest());
+
+            System.out.println("running");
+        }
+
+    }
 }
--- src/main/java/com/test12/javacore/Test12.java
+++ src/main/java/com/test12/javacore/Test12.java
@@ -1,7 +1,17 @@
 package com.test12.javacore;
 
-/**
- * Created by zhanglong on 16/3/30.
- */
 public class Test12 {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 1000; ++i) {
+
+            System.out.println(Thread.currentThread().getName() + ", Number: " + i);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
